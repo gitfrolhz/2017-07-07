69fd0556abf702c4ed9955b16037dcd990403ee1
lhz <15718842217@63.com>
2017-07-07 16:05:40 +0800

    修改test.java

--- src/a1/com/Test.java
+++ src/a1/com/Test.java
@@ -1,7 +1,8 @@
 package a1.com;
 
 public class Test {
-public static void main (string args []{
-system.out.print("git");
-})
+	public static void main(String[] args) {
+		System.out.println("git");
+	}
+
 }
